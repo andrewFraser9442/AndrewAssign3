8012fc26205524dfde171d3e3e7e19014f592f86
Andrew <n01309442@humbermail.ca>
2021-07-17 20:41:25 -0400

    -initial commit -customized .gitignore file

---
+++ AndrewAssign33/src/main/java/andrew/fraser/n01309442/AndrewActivity.java
@@ -0,0 +1,14 @@
+package andrew.fraser.n01309442;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AndrewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
