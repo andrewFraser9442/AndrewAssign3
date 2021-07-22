c006428a2c62cda7bd9c6b3952d5d831dcda5ca7
Andrew <n01309442@humbermail.ca>
2021-07-21 22:05:05 -0400

    -a toast is displayed everytime the user click on the switch

--- AndrewAssign33/src/main/java/andrew/fraser/n01309442/N01309442.java
+++ AndrewAssign33/src/main/java/andrew/fraser/n01309442/N01309442.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class N01309442 extends Fragment {
 
@@ -19,5 +21,17 @@ public class N01309442 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab3, container, false);
+
+        Switch switch = (Switch) findViewById(R.id.switch1);
+
+        switch.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            public void onCheckedChanged(Switch v, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(getApplicationContext(),R.string.switchOn, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),R.string.switchOff, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
